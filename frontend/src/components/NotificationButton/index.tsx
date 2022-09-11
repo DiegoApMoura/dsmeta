78b757fdb8a2a7a75e8a57946048265dfa571d31
Diego Moura <102909704+DiegoApMoura@users.noreply.github.com>
2022-09-11 20:28:04 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -2,6 +2,7 @@ import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 import './styles.css'
 import axios from 'axios'
+import { toast } from 'react-toastify';
 
 type Props = {
   saleId: number;
@@ -9,8 +10,8 @@ type Props = {
 function handleClick(id : number){
 axios(`${BASE_URL}/sales/${id}/notification`)
 .then(response=>{
-  console.log("Sucesso");
-})
+  toast.info("SMS enviado com sucesso");
+});
  
 }
 function NotificationButton({saleId} : Props) {
